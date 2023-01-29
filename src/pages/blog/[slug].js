4f8886d650c3d41075ed0b6db584037ef151db60
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2023-01-30 00:45:20 +0500

    Lets begin again

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,98 +1,104 @@
-import React from 'react'
-import { client, urlFor } from '../../../lib/client'
-import { jsx, Image, Box, Heading, Text, Flex, Link , Grid} from 'theme-ui';
-import { ThemeProvider } from 'theme-ui';
-import { StickyProvider } from '../../contexts/app/app.provider';
-import theme from 'theme';
-import Layout from 'components/layout';
-import CoreFeature from '../../sections/core-feature';
-import Footer from 'components/footer/footer';
-import BlogSection from 'sections/blog-section';
+import React, { useState } from "react";
+import { client, urlFor } from "../../../lib/client";
+import { jsx, Image, Box, Heading, Text, Flex, Link, Grid } from "theme-ui";
+import { ThemeProvider } from "theme-ui";
+import { StickyProvider } from "../../contexts/app/app.provider";
+import theme from "theme";
+import Layout from "components/layout";
+import CoreFeature from "../../sections/core-feature";
+import Footer from "components/footer/footer";
+import BlogSection from "sections/blog-section";
+import Header from "components/header/header";
+import Sticky from "react-stickynode";
+import LogoDark from "assets/isca.png";
+import Logo from "components/logo";
 
 const Blogdetails = ({ blogs, blog }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <StickyProvider>
-                <br /><br />
+  const [isSticky, setIsSticky] = useState(false);
+  const handleStateChange = (status) => {
+    if (status.status === Sticky.STATUS_FIXED) {
+      setIsSticky(true);
+    } else if (status.status === Sticky.STATUS_ORIGINAL) {
+      setIsSticky(false);
+    }
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ marginLeft: "10%" }}>
+        <Logo src={LogoDark} />
+      </div>
 
-                <CoreFeature data={blog} />
+      <StickyProvider>
+        <CoreFeature data={blog} />
 
-                <Box sx={styles.contentBox}>
-
-                    <Grid sx={styles.grid}>
-                        
-                            <Box sx={styles.card} key={blog.id}>
-                                <Box sx={styles.wrapper}>
-                                    <Heading sx={styles.wrapper.title}>{blog.title}</Heading>
-                                    <Text sx={styles.wrapper.subTitle}>{blog.content}</Text>
-                                </Box>
-                            </Box>
-                        
-                    </Grid>
-                </Box>
-<br/><br/>
-                <BlogSection blog={blogs} />
-                <Footer />
-            </StickyProvider>
-        </ThemeProvider>
-    );
-}
-
+        <Box sx={styles.contentBox}>
+          <Grid sx={styles.grid}>
+            <Box sx={styles.card} key={blog.id}>
+              <Box sx={styles.wrapper}>
+                <Heading sx={styles.wrapper.title}>{blog.title}</Heading>
+                <Text sx={styles.wrapper.subTitle}>{blog.content}</Text>
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
+        <br />
+        <br />
+        <BlogSection blog={blogs} />
+        <Footer />
+      </StickyProvider>
+    </ThemeProvider>
+  );
+};
 
 export const getServerSideProps = async ({ params: { slug } }) => {
-    const query = `*[_type == "blog" && slug.current == '${slug}'][0]`;
-    const blogsQuery = '*[_type == "blog"]'
+  const query = `*[_type == "blog" && slug.current == '${slug}'][0]`;
+  const blogsQuery = '*[_type == "blog"]';
 
-    const blog = await client.fetch(query);
-    const blogs = await client.fetch(blogsQuery);
+  const blog = await client.fetch(query);
+  const blogs = await client.fetch(blogsQuery);
 
-    console.log(blog);
+  console.log(blog);
 
-    return {
-        props: { blogs, blog }
-    }
-}
+  return {
+    props: { blogs, blog },
+  };
+};
 
-export default Blogdetails
+export default Blogdetails;
 
 const styles = {
-  
-    contentBox: {
-      width:'70%',
-      flexShrink: 0,
-      mb: [7, null, 60, 0],
-      textAlign: 'center',
-    justifyContent:'center',
-    margin:'auto'
+  contentBox: {
+    width: "70%",
+    flexShrink: 0,
+    mb: [7, null, 60, 0],
+    textAlign: "center",
+    justifyContent: "center",
+    margin: "auto",
+  },
+  grid: {
+    mx: "auto",
+    width: "auto",
+    gridTemplateColumns: ["repeat(1,1fr)"],
+  },
+
+  wrapper: {
+    margin: "auto",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+
+    title: {
+      fontSize: 10,
+      color: "heading_secondary",
+      lineHeight: 1.4,
+      fontWeight: 700,
     },
-    grid: {
-     
-      mx: 'auto',
-      width: 'auto',
-      gridTemplateColumns: ['repeat(1,1fr)'],
+
+    subTitle: {
+      fontSize: [1, null, null, "14px", 1],
+      fontWeight: 400,
+      lineHeight: 1.9,
     },
- 
-    wrapper: {
-        margin:'auto',
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      textAlign: 'center',
-      
-      title: {
-        fontSize: 10,
-        color: 'heading_secondary',
-        lineHeight: 1.4,
-        fontWeight: 700,
-        
-      },
-  
-      subTitle: {
-        fontSize: [1, null, null, '14px', 1],
-        fontWeight: 400,
-        lineHeight: 1.9,
-      },
-    },
- 
-  };
-  
+  },
+};
